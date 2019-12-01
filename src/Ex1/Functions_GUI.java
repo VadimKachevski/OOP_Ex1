225d87c718578537e6dfc73a4a1999ad8bf951a7
mayplex <mayplex123@gmail.com>
2019-12-01 11:55:50 +0200

    Added Function GUI and GSON

--- src/Ex1/Functions_GUI.java
+++ src/Ex1/Functions_GUI.java
@@ -1,16 +1,19 @@
 package Ex1;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+import com.google.gson.Gson;
 
 
 
@@ -120,25 +123,25 @@ public class Functions_GUI implements functions {
 		String line = reader.readLine();
 		while(line != null)
 		{
-//			int[] rgb = new int[3]; // 0 - r , 1 - g , 2 - b
-//			int indexFX= line.indexOf("java.awt.Color");
-//			if(indexFX != -1)
-//			{
-//				String colors = line.substring(line.indexOf('[')+1, line.indexOf(']'));
-//				String[] colorArr = colors.split(",");
-//				for (int i = 0; i < colorArr.length; i++) 
-//				{
-//					String ans ="";
-//					for (int j = 0; j < colorArr[i].length(); j++) {
-//						if(colorArr[i].charAt(j) >= '0' && colorArr[i].charAt(j) <= '9')
-//						{
-//							ans=	ans.concat(""+colorArr[i].charAt(j));
-//						}
-//					}
-//					rgb[i] = Integer.parseInt(ans);
-//					ans="";
-//				}
-//			}
+			//			int[] rgb = new int[3]; // 0 - r , 1 - g , 2 - b
+			//			int indexFX= line.indexOf("java.awt.Color");
+			//			if(indexFX != -1)
+			//			{
+			//				String colors = line.substring(line.indexOf('[')+1, line.indexOf(']'));
+			//				String[] colorArr = colors.split(",");
+			//				for (int i = 0; i < colorArr.length; i++) 
+			//				{
+			//					String ans ="";
+			//					for (int j = 0; j < colorArr[i].length(); j++) {
+			//						if(colorArr[i].charAt(j) >= '0' && colorArr[i].charAt(j) <= '9')
+			//						{
+			//							ans=	ans.concat(""+colorArr[i].charAt(j));
+			//						}
+			//					}
+			//					rgb[i] = Integer.parseInt(ans);
+			//					ans="";
+			//				}
+			//			}
 			line = line.substring(line.indexOf("f(x)=")+"f(x)=".length());
 			line = line.strip();
 			ComplexFunction cf = new ComplexFunction();
@@ -159,9 +162,9 @@ public class Functions_GUI implements functions {
 		int counter = 0;
 		while(itrFunction.hasNext())
 		{
-		//	Color c = itrColor.next();
+			//	Color c = itrColor.next();
 			function f = itrFunction.next();
-		//	fw.write(counter+") "+c.toString()+"   f(x)= "+f.toString());
+			//	fw.write(counter+") "+c.toString()+"   f(x)= "+f.toString());
 			fw.write(counter+") "+"   f(x)= "+f.toString()+'\n');
 			counter++;
 		}
@@ -173,11 +176,11 @@ public class Functions_GUI implements functions {
 	@Override
 	public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
 		//StdDraw.enableDoubleBuffering();
-		
+
 		StdDraw.setCanvasSize(width, height);
 		StdDraw.setXscale(rx.get_min(), rx.get_max());
 		StdDraw.setYscale(ry.get_min(), ry.get_max());
-		
+
 		StdDraw.setPenColor(Color.LIGHT_GRAY);
 		for (int i = 0; i <= resolution; i=i+10) {
 			StdDraw.line(rx.get_min()+i/10, ry.get_min(), rx.get_min()+i/10, ry.get_max());
@@ -185,20 +188,87 @@ public class Functions_GUI implements functions {
 		for (int i = 0; i <= resolution; i=i+10) {
 			StdDraw.line(rx.get_min(), ry.get_min()+i/10, rx.get_max(), ry.get_min()+i/10);
 		}
-			
+
 		StdDraw.setPenColor(Color.BLACK);
 		StdDraw.setPenRadius(0.005);
-		StdDraw.line(rx.get_min(), (ry.get_max()+ry.get_min())/2, rx.get_max(), (ry.get_max()+ry.get_min())/2);
-		StdDraw.line((rx.get_max()+rx.get_min())/2, ry.get_min(), (rx.get_max()+rx.get_min())/2, ry.get_max());
-		
-		
+		//StdDraw.line(rx.get_min(), (ry.get_max()+ry.get_min())/2, rx.get_max(), (ry.get_max()+ry.get_min())/2);
+		StdDraw.setFont(new Font("TimesRoman", Font.BOLD, 15));
+		StdDraw.line(rx.get_min(), 0, rx.get_max(), 0);
+		for (int i = 0; i <= Math.abs(rx.get_min())+Math.abs(rx.get_max()); i++) {
+			StdDraw.text(rx.get_min()+i, -0.30, Double.toString((rx.get_min()+i)));
+		}
+		StdDraw.line(0, ry.get_min(), 0, ry.get_max());
+		for (int i = 0; i <= Math.abs(ry.get_min())+Math.abs(ry.get_max()); i++) {
+			StdDraw.text(-0.30,ry.get_min()+i, (Double.toString((int)(ry.get_min()+i))));
+		}
+
+		for (function function : ColFunctions) {
+			double[] xMAX = new double[resolution+1];
+			double[] yMAX = new double[resolution+1];
+			double[] xMIN = new double[resolution+1];
+			double[] yMIN = new double[resolution+1];
+			for (int i = 0; i <= resolution; i++) {
+				xMAX[i] = rx.get_max() * i / resolution;
+				yMAX[i] = function.f(xMAX[i]);
+
+				xMIN[i] = rx.get_min() * i / resolution;
+				yMIN[i] = function.f(xMIN[i]);
+			}
+			int R = (int)(Math.random()*256);
+			int G = (int)(Math.random()*256);
+			int B= (int)(Math.random()*256);
+			Color color = new Color(R, G, B);
+			StdDraw.setPenColor(color);
+			for (int i = 0; i < resolution; i++) {
+				StdDraw.line(xMAX[i], yMAX[i], xMAX[i+1], yMAX[i+1]);
+				StdDraw.line(xMIN[i], yMIN[i], xMIN[i+1], yMIN[i+1]);
+			}
+
+		}
+		StdDraw.setPenColor(Color.BLACK);
 
 	}
 
 	@Override
 	public void drawFunctions(String json_file) {
-		// TODO Auto-generated method stub
+		//		// TODO Auto-generated method stub
+		//		InputStream fis = new FileInputStream(JSON_FILE);
+		//
+		//		//create JsonReader object
+		//		JsonReader jsonReader = Json.createReader(fis);
+		//
+		//		/**
+		//		 * We can create JsonReader from Factory also
+		//		JsonReaderFactory factory = Json.createReaderFactory(null);
+		//		jsonReader = factory.createReader(fis);
+		//		 */
+		//
+		//		//get JsonObject from JsonReader
+		//		JsonObject jsonObject = jsonReader.readObject();
+		//
+		//		//we can close IO resource and JsonReader now
+		//		jsonReader.close();
+		//		fis.close();
+		Gson gs = new Gson();
+		GUI_PARAMS gp = gs.fromJson(json_file, GUI_PARAMS.class);
+		drawFunctions(gp.Width, gp.Height, new Range(gp.Range_X[0], gp.Range_X[1]), new Range(gp.Range_Y[0], gp.Range_Y[1]), gp.Resolution);
 
 	}
 
+}
+class GUI_PARAMS
+{
+	int Width;
+	int Height;
+	int Resolution;
+	int[] Range_X;
+	int[] Range_Y;
+	public GUI_PARAMS(int Width,int Height,int Resolution,int[] Range_X,int[] Range_Y)
+	{
+		this.Width = Width;
+		this.Height = Height;
+		this.Resolution = Resolution;
+		this.Range_X = Range_X;
+		this.Range_Y = Range_Y;
+	}
 }
